0574fb809ef93f225d1bf0ff86e005ac2848d40e
Cheryl Tan <cheryltan134@gmail.com>
2024-01-13 13:08:54 +0800

    finished CREATE functionality

--- models/plans.js
+++ models/plans.js
@@ -1,12 +1,53 @@
-const tripDao = require("../daos/trip");
+const { Plan, Trip } = require("../daos/trip");
 
-module.exports = { createOne, getOne, getAll, updateOne, deleteOne };
+module.exports = { 
+    findTripById, 
+    createOne,
+ };
 
-function createOne() {}
+async function createOne(plans, tripID) {
+    try {
+        const newPlan = await Plan.create({
+            header: plans.header,
+            description: plans.description,
+            tripID: tripID,
+          });
 
-function getOne() {}
-function getAll() {}
+        const trip = await Trip.findById(tripID);
+        if (!trip) {
+            throw new Error('Trip not found'); // Handle the error accordingly
+        }
 
-function updateOne() {}
+        // Push the newPlan to the trip's plans array
+        trip.plans.push(newPlan);
+        await trip.save();
 
-function deleteOne() {}
+        console.log("Plan created:", newPlan);
+    } catch (error) {
+        console.error("Error creating plan:", error);
+    }
+  }
+
+async function findTripById(tripID) {
+try {
+    const trip = await Trip.findById(tripID);
+
+    if (!trip) {
+    throw new Error('Trip not found');
+    }
+
+    return trip;
+} catch (error) {
+    throw error; // Propagate the error to the caller
+}
+}
+  
+
+// function getOne() {}
+// function getAll() {
+//     const plans = tripDao.find({title: param})
+// }
+
+// function updateOne() {}
+
+// function deleteOne() {}
